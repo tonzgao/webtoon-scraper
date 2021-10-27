628a2550d2e2c6311c3a1073fd9256210f6d19b7
Tony Gao <tonyzgao@gmail.com>
2021-10-27 17:11:12 -0400

    fix incorrect polling method

--- src/scrapers/toonily/index.ts
+++ src/scrapers/toonily/index.ts
@@ -5,6 +5,10 @@ import { runTimeout } from '../../helpers';
 
 import { HeadlessScraper } from '../common'
 
+const titleElement = '.post-title';
+const chapterElement = '.wp-manga-chapter a'
+const imageElement = '.wp-manga-chapter-img'
+
 export const isToonily = (url: string) => {
   // match regex
   return true;
@@ -18,51 +22,68 @@ export class Toonily extends HeadlessScraper {
   }> {
     await this.page.goto(url, { waitUntil: 'load' })
     await this.click('.btn-adult-confirm', { require: false })
-    await this.page.waitForSelector('.post-title');
-    const title = await this.page.$eval('.post-title', el => el.textContent) as string;
-    const chapters = await this.page.$$eval('.wp-manga-chapter a', els => els.map(a => (a as HTMLAnchorElement).href))
+    await this.page.waitForSelector(titleElement);
+    const title = await this.page.$eval(titleElement, el => el.textContent) as string;
+    const chapters = await this.page.$$eval(chapterElement, els => els.map(a => (a as HTMLAnchorElement).href))
     return {
       series: title.trim(),
       chapters: chapters.reverse(),
     }
   }
 
+  // Get all image elements
+  private async getImages() {
+    return await this.page.$$(imageElement)
+  }
+
+  // Check if src is loaded from specified image element
+  private async getSource(n: number) {
+    const images = await this.getImages();
+    const image = images[n] as ElementHandle<HTMLImageElement>
+    const loaded = await image.evaluate((el) => {
+      const source = el.src
+      el.scrollIntoView()
+      return Boolean(source);
+    })
+    if (!loaded) {
+      await this.wait(100);
+    }
+    return image;
+  }
+
+  // Wait until src is loaded from image element
+  private async pollSource(n: number) {
+    let image = await this.getSource(n);
+    while (!image) {
+      image = await this.getSource(n);
+    }
+    return image;
+  }
+
+  // Scrape image in a chapter
+  private async scrapeLink(n: number) {
+    try {
+      const result = await runTimeout(1000, () => this.pollSource(n))
+      return {
+        image: result,
+        number: n,
+      };
+    } catch (e) {
+      if (this.options.strict) {
+        throw e
+      }
+      console.debug(e)
+      return {
+        image: undefined,
+        number: n
+      }
+    }
+  }
+
   // Scrape all images in a chapter
   protected async *scrapeLinks() {
-    const getImages = async () => await this.page.$$('.wp-manga-chapter-img')
-
-    const pollSource = async (n: number) => {
-      const images = await getImages();
-      const image = images[n] as ElementHandle<HTMLImageElement>
-      const loaded = await image.evaluate((el) => {
-        const source = el.src
-        el.scrollIntoView()
-        return Boolean(source);
-      })
-      if (!loaded) {
-        await this.wait(100);
-      }
-      return image;
-    }
-
-    for (const i of range((await getImages()).length)) {
-      try {
-        const result = await runTimeout(1000, () => pollSource(i))
-        yield {
-          image: result,
-          number: i,
-        };
-      } catch (e) {
-        if (this.options.strict) {
-          throw e
-        }
-        console.debug(e)
-        yield {
-          image: undefined,
-          number: i
-        }
-      }
-
+    for (const i of range((await this.getImages()).length)) {
+      yield await this.scrapeLink(i);
     }
   }
 
